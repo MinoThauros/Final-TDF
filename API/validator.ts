73ad881be426ccb746feab188de81442b8e717ce
MinoThauros <herimino@timatech.ca>
2023-08-24 22:00:22 -0400

    fixed date component to only take an intelligible format of date

--- API/validator.ts
+++ API/validator.ts
@@ -1,6 +1,8 @@
 //creating a validator which takes in an array
 
 export class Validator {
+  // Regular expression to match the yyyy-mm-dd format,
+  // limit the month to 01-12, the year to 1900-2023, and the day to 01-31
   private readonly   specialChars = /[`!@#$%^&*_+\-=\[\]{};':"\\|,.<>\/?~]/;
   private readonly emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Simple email regex for demonstration purposes
   private readonly dateRegex = /^(19[0-9]{2}|20[0-2][0-3])-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
